14f7496b77ca4cae70fa7f546adfdb957dafe4ac
Ian M. Kerman <neodenium@gmail.com>
2014-09-18 01:47:50 -0700

    Benchmark Updates

--- Project1/src/edu/gatech/cs6310/project1/test/Benchmark_Heap.java
+++ Project1/src/edu/gatech/cs6310/project1/test/Benchmark_Heap.java
@@ -1,8 +1,5 @@
 package edu.gatech.cs6310.project1.test;
 
-import static org.junit.Assert.*;
-
-import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import org.junit.After;
@@ -23,6 +20,114 @@ public class Benchmark_Heap extends AbstractBenchmark{
 	
 	/*****************************************
 	 * 
+	 * Set of Memory Tests for Matrix Nodes
+	 * 
+	 */
+	@Test
+	public void matrixNodes_100x100_T100() {
+		System.out.print("*"+this.getClass().getSimpleName()+".matrixNodes_100x100_T100,");
+		disableOutput();
+		Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args100x100T100);
+		enableOutput();
+	}
+	
+	@Test
+	public void matrixNodes_100x100_T10() {
+		System.out.print("*"+this.getClass().getSimpleName()+".matrixNodes_100x100_T10,");
+		disableOutput();
+		Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args100x100T10);
+		enableOutput();
+	}
+	
+	@Test
+	public void matrixNodes_100x100_T1() {
+		System.out.print("*"+this.getClass().getSimpleName()+".matrixNodes_100x100_T1,");
+		disableOutput();
+		//Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args100x100T1);
+		enableOutput();
+	}
+	
+	@Test
+	public void matrixNodes_10x10_T100() {
+		System.out.print("*"+this.getClass().getSimpleName()+".matrixNodes_10x10_T100,");
+		disableOutput();
+		Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args10x10T100);
+		enableOutput();
+	}
+	
+	@Test
+	public void matrixNodes_10x10_T10() {
+		System.out.print("*"+this.getClass().getSimpleName()+".matrixNodes_10x10_T10,");
+		disableOutput();
+		Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args10x10T10);
+		enableOutput();
+	}
+	
+	@Test
+	public void matrixNodes_10x10_T1() {
+		System.out.print("*"+this.getClass().getSimpleName()+".matrixNodes_10x10_T1,");
+		disableOutput();
+		Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args10x10T1);
+		enableOutput();
+	}
+	
+	@Test
+	public void matrixNodes_3x3_T10() {
+		System.out.print("*"+this.getClass().getSimpleName()+".matrixNodes_3x3_T10,");
+		disableOutput();
+		Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args3x3T10);
+		enableOutput();
+	}
+	
+	@Test
+	public void matrixNodes_3x3_T100() {
+		System.out.print("*"+this.getClass().getSimpleName()+".matrixNodes_3x3_T100,");
+		disableOutput();
+		Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args3x3T100);
+		enableOutput();
+	}
+	
+	@Test
+	public void matrixNodes_3x3_T1() {
+		System.out.print("*"+this.getClass().getSimpleName()+".matrixNodes_3x3_T1,");
+		disableOutput();
+		Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args3x3T1);
+		enableOutput();
+	}
+	
+	@Test
+	public void matrixNodes_10x10_T1T10() {
+		System.out.print("*"+this.getClass().getSimpleName()+".matrixNodes_10x10_T1T10,");
+		disableOutput();
+		Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args10x10T1T10);
+		enableOutput();
+	}
+	
+	@Test
+	public void matrixNodes_10x10_T10T100() {
+		System.out.print("*"+this.getClass().getSimpleName()+".matrixNodes_10x10_T10T100,");
+		disableOutput();
+		Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args10x10T10T100);
+		enableOutput();
+	}
+	
+	@Test
+	public void matrixNodes_10x10_T50T100() {
+		System.out.print("*"+this.getClass().getSimpleName()+".matrixNodes_10x10_T50T100,");
+		disableOutput();
+		new Tpdohp.Demo(Constants.args10x10T50T100);
+		enableOutput();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	/*****************************************
+	 * 
 	 * Set of Memory Tests for Wrapped Float
 	 * 
 	 */
@@ -35,10 +140,42 @@ public class Benchmark_Heap extends AbstractBenchmark{
 	}
 	
 	@Test
+	public void wrappedFloat_100x100_T10() {
+		System.out.print("*"+this.getClass().getSimpleName()+".wrappedFloat_100x100_T10,");
+		disableOutput();
+		Twfahp.Demo demo = new Twfahp.Demo(Constants.args100x100T10);
+		enableOutput();
+	}
+	
+	@Test
+	public void wrappedFloat_100x100_T1() {
+		System.out.print("*"+this.getClass().getSimpleName()+".wrappedFloat_100x100_T1,");
+		disableOutput();
+		Twfahp.Demo demo = new Twfahp.Demo(Constants.args100x100T1);
+		enableOutput();
+	}
+	
+	@Test
+	public void wrappedFloat_10x10_T100() {
+		System.out.print("*"+this.getClass().getSimpleName()+".wrappedFloat_10x10_T100,");
+		disableOutput();
+		Twfahp.Demo demo = new Twfahp.Demo(Constants.args10x10T100);
+		enableOutput();
+	}
+	
+	@Test
 	public void wrappedFloat_10x10_T10() {
 		System.out.print("*"+this.getClass().getSimpleName()+".wrappedFloat_10x10_T10,");
 		disableOutput();
 		Twfahp.Demo demo = new Twfahp.Demo(Constants.args10x10T10);
+		enableOutput();
+	}
+	
+	@Test
+	public void wrappedFloat_10x10_T1() {
+		System.out.print("*"+this.getClass().getSimpleName()+".wrappedFloat_10x10_T1,");
+		disableOutput();
+		Twfahp.Demo demo = new Twfahp.Demo(Constants.args10x10T1);
 		enableOutput();
 	}
 	
@@ -91,15 +228,6 @@ public class Benchmark_Heap extends AbstractBenchmark{
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	/*****************************************
 	 * 
 	 * Set of Memory Tests for Primitive Float
@@ -114,10 +242,42 @@ public class Benchmark_Heap extends AbstractBenchmark{
 	}
 	
 	@Test
+	public void primitiveFloat_100x100_T10() {
+		System.out.print("*"+this.getClass().getSimpleName()+".primitiveFloat_100x100_T10,");
+		disableOutput();
+		Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args100x100T10);
+		enableOutput();
+	}
+	
+	@Test
+	public void primitiveFloat_100x100_T1() {
+		System.out.print("*"+this.getClass().getSimpleName()+".primitiveFloat_100x100_T1,");
+		disableOutput();
+		Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args100x100T1);
+		enableOutput();
+	}
+	
+	@Test
+	public void primitiveFloat_10x10_T100() {
+		System.out.print("*"+this.getClass().getSimpleName()+".primitiveFloat_10x10_T100,");
+		disableOutput();
+		Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args10x10T100);
+		enableOutput();
+	}
+	
+	@Test
 	public void primitiveFloat_10x10_T10() {
 		System.out.print("*"+this.getClass().getSimpleName()+".primitiveFloat_10x10_T10,");
 		disableOutput();
 		Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args10x10T10);
+		enableOutput();
+	}
+	
+	@Test
+	public void primitiveFloat_10x10_T1() {
+		System.out.print("*"+this.getClass().getSimpleName()+".primitiveFloat_10x10_T1,");
+		disableOutput();
+		Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args10x10T1);
 		enableOutput();
 	}
 	
@@ -169,6 +329,7 @@ public class Benchmark_Heap extends AbstractBenchmark{
 		enableOutput();
 	}
 	
+	
 	/******************************************
 	 * 
 	 * Set of Memory Tests for Primitive Double
@@ -183,10 +344,42 @@ public class Benchmark_Heap extends AbstractBenchmark{
 	}
 	
 	@Test
+	public void primitiveDouble_100x100_T10() {
+		System.out.print("*"+this.getClass().getSimpleName()+".primitiveDouble_100x100_T10,");
+		disableOutput();
+		Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args100x100T10);
+		enableOutput();
+	}
+	
+	@Test
+	public void primitiveDouble_100x100_T1() {
+		System.out.print("*"+this.getClass().getSimpleName()+".primitiveDouble_100x100_T1,");
+		disableOutput();
+		Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args100x100T1);
+		enableOutput();
+	}
+	
+	@Test
+	public void primitiveDouble_10x10_T100() {
+		System.out.print("*"+this.getClass().getSimpleName()+".primitiveDouble_10x10_T100,");
+		disableOutput();
+		Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args10x10T100);
+		enableOutput();
+	}
+	
+	@Test
 	public void primitiveDouble_10x10_T10() {
 		System.out.print("*"+this.getClass().getSimpleName()+".primitiveDouble_10x10_T10,");
 		disableOutput();
 		Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args10x10T10);
+		enableOutput();
+	}
+	
+	@Test
+	public void primitiveDouble_10x10_T1() {
+		System.out.print("*"+this.getClass().getSimpleName()+".primitiveDouble_10x10_T1,");
+		disableOutput();
+		Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args10x10T1);
 		enableOutput();
 	}
 	
--- Project1/src/edu/gatech/cs6310/project1/test/Benchmark_callgcTrue_15Rounds_5Warmup.java
+++ Project1/src/edu/gatech/cs6310/project1/test/Benchmark_callgcTrue_15Rounds_5Warmup.java
@@ -1,9 +1,5 @@
 package edu.gatech.cs6310.project1.test;
 
-import static org.junit.Assert.*;
-
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
 import java.io.PrintStream;
 
 import org.junit.After;
@@ -37,25 +33,287 @@ public class Benchmark_callgcTrue_15Rounds_5Warmup extends AbstractBenchmark {
 	public void tearDown() throws Exception {
 		System.setOut(original);
 	}
-
+	
 	@Test
 	public void benchmark_ThreadSleep100() throws InterruptedException {
 		Thread.sleep(100);
 	}
 	
 	@Test
-	public void benchmark_ThreadSleep10() throws InterruptedException {
-		Thread.sleep(10);
-	}
-
-	@Test
-	public void benchmark_PrimitiveDoubleDemo10() {
-	    Tpdahp.Demo primitiveDoubleDemo = new Tpdahp.Demo(Constants.args10x10T10);
+	public void benchmark_ThreadSleep1000() throws InterruptedException {
+		Thread.sleep(1000);
 	}
 	
 	@Test
-	public void benchmark_PrimitiveDoubleDemo100() {
-	    Tpdahp.Demo primitiveDoubleDemo = new Tpdahp.Demo(Constants.args100x100T100);
+	public void benchmark_ThreadSleep10() throws InterruptedException {
+		Thread.sleep(10);
+	}
+	
+	
+	/******************************************
+	 * 
+	 * Set of Memory Tests for Primitive Double
+	 * 
+	 */
+	
+	@Test
+	public void benchmark_PrimitiveDouble_3x3_T100() {
+	    Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args3x3T100);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveDouble_3x3_T10() {
+	    Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args3x3T10);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveDouble_3x3_T1() {
+	    Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args3x3T1);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveDouble_10x10_T100() {
+	    Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args10x10T100);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveDouble_10x10_T10() {
+	    Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args10x10T10);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveDouble_10x10_T1() {
+	    Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args10x10T1);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveDouble_100x100_T100() {
+	    Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args100x100T100);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveDouble_100x100_T10() {
+	    Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args100x100T10);
 	}
 
+	@Test
+	public void benchmark_PrimitiveDouble_100x100_T1() {
+	    Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args100x100T1);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveDouble_10x10_T1T10() {
+	    Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args10x10T1T10);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveDouble_10x10_T10T100() {
+	    Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args10x10T10T100);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveDouble_10x10_T50T100() {
+	    Tpdahp.Demo demo = new Tpdahp.Demo(Constants.args10x10T50T100);
+	}
+	
+	
+	/******************************************
+	 * 
+	 * Set of Memory Tests for Primitive Float
+	 * 
+	 */
+	
+	@Test
+	public void benchmark_PrimitiveFloat_3x3_T100() {
+	    Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args3x3T100);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveFloat_3x3_T10() {
+	    Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args3x3T10);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveFloat_3x3_T1() {
+	    Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args3x3T1);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveFloat_10x10_T100() {
+	    Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args10x10T100);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveFloat_10x10_T10() {
+	    Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args10x10T10);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveFloat_10x10_T1() {
+	    Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args10x10T1);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveFloat_100x100_T100() {
+	    Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args100x100T100);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveFloat_100x100_T10() {
+	    Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args100x100T10);
+	}
+
+	@Test
+	public void benchmark_PrimitiveFloat_100x100_T1() {
+	    Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args100x100T1);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveFloat_10x10_T1T10() {
+	    Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args10x10T1T10);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveFloat_10x10_T10T100() {
+	    Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args10x10T10T100);
+	}
+	
+	@Test
+	public void benchmark_PrimitiveFloat_10x10_T50T100() {
+	    Tpfahp.Demo demo = new Tpfahp.Demo(Constants.args10x10T50T100);
+	}
+	
+	
+	/******************************************
+	 * 
+	 * Set of Memory Tests for Wrapped Float
+	 * 
+	 */
+	
+	@Test
+	public void benchmark_WrappedFloat_3x3_T100() {
+	    Twfahp.Demo demo = new Twfahp.Demo(Constants.args3x3T100);
+	}
+	
+	@Test
+	public void benchmark_WrappedFloat_3x3_T10() {
+	    Twfahp.Demo demo = new Twfahp.Demo(Constants.args3x3T10);
+	}
+	
+	@Test
+	public void benchmark_WrappedFloat_3x3_T1() {
+	    Twfahp.Demo demo = new Twfahp.Demo(Constants.args3x3T1);
+	}
+	
+	@Test
+	public void benchmark_WrappedFloat_10x10_T100() {
+	    Twfahp.Demo demo = new Twfahp.Demo(Constants.args10x10T100);
+	}
+	
+	@Test
+	public void benchmark_WrappedFloat_10x10_T10() {
+	    Twfahp.Demo demo = new Twfahp.Demo(Constants.args10x10T10);
+	}
+	
+	@Test
+	public void benchmark_WrappedFloat_10x10_T1() {
+	    Twfahp.Demo demo = new Twfahp.Demo(Constants.args10x10T1);
+	}
+	
+	@Test
+	public void benchmark_WrappedFloat_100x100_T100() {
+	    Twfahp.Demo demo = new Twfahp.Demo(Constants.args100x100T100);
+	}
+	
+	@Test
+	public void benchmark_WrappedFloat_100x100_T10() {
+	    Twfahp.Demo demo = new Twfahp.Demo(Constants.args100x100T10);
+	}
+
+	@Test
+	public void benchmark_WrappedFloat_100x100_T1() {
+	    Twfahp.Demo demo = new Twfahp.Demo(Constants.args100x100T1);
+	}
+	
+	@Test
+	public void benchmark_WrappedFloat_10x10_T1T10() {
+	    Twfahp.Demo demo = new Twfahp.Demo(Constants.args10x10T1T10);
+	}
+	
+	@Test
+	public void benchmark_WrappedFloat_10x10_T10T100() {
+	    Twfahp.Demo demo = new Twfahp.Demo(Constants.args10x10T10T100);
+	}
+	
+	@Test
+	public void benchmark_WrappedFloat_10x10_T50T100() {
+	    Twfahp.Demo demo = new Twfahp.Demo(Constants.args10x10T50T100);
+	}
+	
+	
+	/******************************************
+	 * 
+	 * Set of Memory Tests for Matrix Nodes
+	 * 
+	 */
+	
+	@Test
+	public void benchmark_MatrixNodes_3x3_T100() {
+	    Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args3x3T100);
+	}
+	
+	@Test
+	public void benchmark_MatrixNodes_3x3_T10() {
+	    Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args3x3T10);
+	}
+	
+	@Test
+	public void benchmark_MatrixNodes_3x3_T1() {
+	    Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args3x3T1);
+	}
+	
+	@Test
+	public void benchmark_MatrixNodes_10x10_T100() {
+	    Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args10x10T100);
+	}
+	
+	@Test
+	public void benchmark_MatrixNodes_10x10_T10() {
+	    Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args10x10T10);
+	}
+	
+	@Test
+	public void benchmark_MatrixNodes_10x10_T1() {
+	    Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args10x10T1);
+	}
+	
+	@Test
+	public void benchmark_MatrixNodes_100x100_T100() {
+	    Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args100x100T100);
+	}
+	
+	@Test
+	public void benchmark_MatrixNodes_100x100_T10() {
+	    Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args100x100T10);
+	}
+
+	@Test
+	public void benchmark_MatrixNodes_100x100_T1() {
+	    Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args100x100T1);
+	}
+	
+	@Test
+	public void benchmark_MatrixNodes_10x10_T1T10() {
+	    Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args10x10T1T10);
+	}
+	
+	@Test
+	public void benchmark_MatrixNodes_10x10_T10T100() {
+	    Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args10x10T10T100);
+	}
+	
+	@Test
+	public void benchmark_MatrixNodes_10x10_T50T100() {
+	    Tpdohp.Demo demo = new Tpdohp.Demo(Constants.args10x10T50T100);
+	}
 }
